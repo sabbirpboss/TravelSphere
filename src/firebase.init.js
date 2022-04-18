a0eae49e0150e863d5d235d4cb93bd04e76244be
sabbirpboss <sabbir.ah799@gmail.com>
2022-04-18 21:59:33 +0600

    component created

---
+++ src/firebase.init.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAv87kM-TCrcxv9REmQO9euSNUsmtOCr3I",
+  authDomain: "travelsphere-18293.firebaseapp.com",
+  projectId: "travelsphere-18293",
+  storageBucket: "travelsphere-18293.appspot.com",
+  messagingSenderId: "722701188885",
+  appId: "1:722701188885:web:c15afb3d0c9cb535f8f31c",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export default app;
